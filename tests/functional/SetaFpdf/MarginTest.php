4d40211d5ca34b1a1aeda76cb9406808a2889465
Jan Slabon <jan.slabon@setasign.com>
2019-03-13 14:24:19 +0100

    Create MarginTest.php

---
+++ tests/functional/SetaFpdf/MarginTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace setasign\tests\visual\SetaFpdf\Single;
+
+use setasign\tests\TestCase;
+
+class MarginTest extends TestCase
+{
+    public function testResetToMarginsOnPageBreak()
+    {
+        $proxy = $this->getProxy();
+
+        $proxy->SetMargins(2, 5);
+        $proxy->AddPage();
+        $this->assertSame(2, $proxy->GetX());
+        $this->assertSame(5, $proxy->GetY());
+
+        $proxy->SetXY(50, 60);
+
+        $proxy->AddPage();
+        $this->assertSame(2, $proxy->GetX());
+        $this->assertSame(5, $proxy->GetY());
+
+        $proxy->SetMargins(4, 15);
+        $proxy->AddPage();
+        $this->assertSame(4, $proxy->GetX());
+        $this->assertSame(15, $proxy->GetY());
+    }
+}
