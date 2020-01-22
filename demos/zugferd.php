4b92450676c6afae80521e895c9a47567df57a4a
Jan Slabon <jan.slabon@setasign.com>
2020-01-22 22:17:54 +0100

    Fixed ZUGFeRD demo

--- demos/zugferd.php
+++ demos/zugferd.php
@@ -38,10 +38,15 @@ class ZUGFeRD extends SetaFpdi
         } else {
             $info->setCreationDate(new \SetaPDF_Core_DataStructure_Date());
         }
-        $info->setTrapped(\SetaPDF_Core_Document_Info::TRAPPED_TRUE);
 
         $info->updateXmp('http://www.aiim.org/pdfa/ns/id/', 'part', '3');
         $info->updateXmp('http://www.aiim.org/pdfa/ns/id/', 'conformance', 'B');
+
+        $info->xmlAliases['urn:ferd:pdfa:CrossIndustryDocument:invoice:1p0#'] = 'zf';
+        $info->updateXmp('urn:ferd:pdfa:CrossIndustryDocument:invoice:1p0#', 'ConformanceLevel', 'BASIC');
+        $info->updateXmp('urn:ferd:pdfa:CrossIndustryDocument:invoice:1p0#', 'DocumentFileName', 'ZUGFeRD-invoice.xml');
+        $info->updateXmp('urn:ferd:pdfa:CrossIndustryDocument:invoice:1p0#', 'DocumentType', 'INVOICE');
+        $info->updateXmp('urn:ferd:pdfa:CrossIndustryDocument:invoice:1p0#', 'Version', '1.0');
 
         $fileId = \SetaPDF_Core_Type_HexString::str2hex($document->getFileIdentifier(true));
         $uuid = 'uuid:' . substr($fileId, 0, 8) . '-' . substr($fileId, 8, 4) . '-' . substr($fileId, 12, 4) . '-'
@@ -68,14 +73,6 @@ class ZUGFeRD extends SetaFpdi
         $dict = $document->getCatalog()->getDictionary();
         $dict['AF'] = new \SetaPDF_Core_Type_Array([$object]);
 
-        $info = $document->getInfo();
-
-        $info->xmlAliases['urn:ferd:pdfa:CrossIndustryDocument:invoice:1p0#'] = 'zf';
-        $info->updateXmp('urn:ferd:pdfa:CrossIndustryDocument:invoice:1p0#', 'ConformanceLevel', 'BASIC');
-        $info->updateXmp('urn:ferd:pdfa:CrossIndustryDocument:invoice:1p0#', 'DocumentFileName', 'ZUGFeRD-invoice.xml');
-        $info->updateXmp('urn:ferd:pdfa:CrossIndustryDocument:invoice:1p0#', 'DocumentType', 'INVOICE');
-        $info->updateXmp('urn:ferd:pdfa:CrossIndustryDocument:invoice:1p0#', 'Version', '1.0');
-
         $xml = $info->getMetadata();
 
         $description =  $xml->createElementNS('http://www.w3.org/1999/02/22-rdf-syntax-ns#', 'Description');
@@ -96,6 +93,31 @@ class ZUGFeRD extends SetaFpdi
         $li->setAttributeNS('http://www.w3.org/1999/02/22-rdf-syntax-ns#', 'rdf:parseType', 'Resource');
         $bag->appendChild($li);
 
+        $namespaceURI = $xml->createElementNS('http://www.aiim.org/pdfa/ns/schema#', 'namespaceURI', 'http://ns.adobe.com/pdf/1.3/');
+        $li->appendChild($namespaceURI);
+        $prefix = $xml->createElementNS('http://www.aiim.org/pdfa/ns/schema#', 'prefix', 'pdf');
+        $li->appendChild($prefix);
+        $schema = $xml->createElementNS('http://www.aiim.org/pdfa/ns/schema#', 'schema', 'Adobe PDF Schema');
+        $li->appendChild($schema);
+
+        $property = $xml->createElementNS('http://www.aiim.org/pdfa/ns/schema#', 'property');
+        $li->appendChild($property);
+        $seq = $xml->createElementNS('http://www.w3.org/1999/02/22-rdf-syntax-ns#', 'rdf:Seq');
+        $property->appendChild($seq);
+        $li = $xml->createElementNS('http://www.w3.org/1999/02/22-rdf-syntax-ns#', 'rdf:li');
+        $li->setAttributeNS('http://www.w3.org/1999/02/22-rdf-syntax-ns#', 'rdf:parseType', 'Resource');
+        $seq->appendChild($li);
+
+        $category = $xml->createElementNS('http://www.aiim.org/pdfa/ns/property#', 'category', 'internal');
+        $li->appendChild($category);
+        $description = $xml->createElementNS('http://www.aiim.org/pdfa/ns/property#', 'description', 'A name object indicating whether the document has been modified to include trapping information');
+        $li->appendChild($description);
+        $name = $xml->createElementNS('http://www.aiim.org/pdfa/ns/property#', 'name', 'Trapped');
+        $li->appendChild($name);
+        $valueType = $xml->createElementNS('http://www.aiim.org/pdfa/ns/property#', 'valueType', 'Text');
+        $li->appendChild($valueType);
+
+        // ZUGFeRD Schema
         $li = $xml->createElementNS('http://www.w3.org/1999/02/22-rdf-syntax-ns#', 'rdf:li');
         $li->setAttributeNS('http://www.w3.org/1999/02/22-rdf-syntax-ns#', 'rdf:parseType', 'Resource');
         $bag->appendChild($li);
@@ -158,73 +180,7 @@ class ZUGFeRD extends SetaFpdi
         $li->appendChild($category);
         $description = $xml->createElementNS('http://www.aiim.org/pdfa/ns/property#', 'description', 'The conformance level of the ZUGFeRD data');
         $li->appendChild($description);
-
-        $info->syncMetadata();
-
-        // fix Trapped property
-        $xml = $info->getMetadata();
-
-        $description =  $xml->createElementNS('http://www.w3.org/1999/02/22-rdf-syntax-ns#', 'Description');
-        // "[...]an empty string, which means that the XMP is physically local to the resource being described.[...]"
-        $description->setAttribute('rdf:about', '');
-        $description->setAttributeNS(
-            'http://www.w3.org/2000/xmlns/',
-            'xmlns:pdfaExtension',
-            'http://www.aiim.org/pdfa/ns/extension/'
-        );
-        $description->setAttributeNS(
-            'http://www.w3.org/2000/xmlns/',
-            'xmlns:pdfaSchema',
-            'http://www.aiim.org/pdfa/ns/schema#'
-        );
-        $description->setAttributeNS(
-            'http://www.w3.org/2000/xmlns/',
-            'xmlns:pdfaProperty',
-            'http://www.aiim.org/pdfa/ns/property#'
-        );
-
-        $rdf = $xml->getElementsByTagName('RDF')->item(0);
-        $rdf->appendChild($description);
-
-        $schemas = $xml->createElementNS('http://www.aiim.org/pdfa/ns/extension/', 'schemas');
-        $description->appendChild($schemas);
-        $bag = $xml->createElementNS('http://www.w3.org/1999/02/22-rdf-syntax-ns#', 'Bag');
-        $schemas->appendChild($bag);
-        $li = $xml->createElementNS('http://www.w3.org/1999/02/22-rdf-syntax-ns#', 'rdf:li');
-        $li->setAttributeNS('http://www.w3.org/1999/02/22-rdf-syntax-ns#', 'rdf:parseType', 'Resource');
-        $bag->appendChild($li);
-
-        $namespaceURI = $xml->createElementNS(
-            'http://www.aiim.org/pdfa/ns/schema#',
-            'namespaceURI',
-            'http://ns.adobe.com/pdf/1.3/'
-        );
-        $li->appendChild($namespaceURI);
-        $prefix = $xml->createElementNS('http://www.aiim.org/pdfa/ns/schema#', 'prefix', 'pdf');
-        $li->appendChild($prefix);
-        $schema = $xml->createElementNS('http://www.aiim.org/pdfa/ns/schema#', 'schema', 'Adobe PDF Schema');
-        $li->appendChild($schema);
-
-        $property = $xml->createElementNS('http://www.aiim.org/pdfa/ns/schema#', 'property');
-        $li->appendChild($property);
-        $seq = $xml->createElementNS('http://www.w3.org/1999/02/22-rdf-syntax-ns#', 'rdf:Seq');
-        $property->appendChild($seq);
-        $li = $xml->createElementNS('http://www.w3.org/1999/02/22-rdf-syntax-ns#', 'rdf:li');
-        $li->setAttributeNS('http://www.w3.org/1999/02/22-rdf-syntax-ns#', 'rdf:parseType', 'Resource');
-        $seq->appendChild($li);
-
-        $category = $xml->createElementNS('http://www.aiim.org/pdfa/ns/property#', 'category', 'internal');
-        $li->appendChild($category);
-        $description = $xml->createElementNS(
-            'http://www.aiim.org/pdfa/ns/property#',
-            'description',
-            'A name object indicating whether the document has been modified to include trapping information'
-        );
-        $li->appendChild($description);
-        $name = $xml->createElementNS('http://www.aiim.org/pdfa/ns/property#', 'name', 'Trapped');
-        $li->appendChild($name);
-        $valueType = $xml->createElementNS('http://www.aiim.org/pdfa/ns/property#', 'valueType', 'Text');
-        $li->appendChild($valueType);
+        // ZUGFeRD end
 
         // save the updated metadata back to the PDF
         $info->syncMetadata();
