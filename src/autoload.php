04837193232c25c71cd48732bb72aad39e1afe3d
Maximilian Kresse <545671+maximiliankresse@users.noreply.github.com>
2020-08-11 15:04:14 +0200

    Fixed fallback autoloader Fixed composer.lock to be compatible with php5.6

--- src/autoload.php
+++ src/autoload.php
@@ -7,11 +7,15 @@
  * @license   http://opensource.org/licenses/mit-license The MIT License
  */
 
+/**
+ * This file is only a fallback if you aren't using the composer autoloader
+ */
 spl_autoload_register(function ($class) {
-    if (strpos($class, 'setasign\SetaFpdf\\') === 0) {
-        $filename = str_replace('\\', DIRECTORY_SEPARATOR, substr($class, 14)) . '.php';
+    if (strpos($class, 'setasign\\SetaFpdf\\') === 0) {
+        $filename = str_replace('\\', DIRECTORY_SEPARATOR, substr($class, 17)) . '.php';
         $fullpath = __DIR__ . DIRECTORY_SEPARATOR . $filename;
-        if (file_exists($fullpath)) {
+
+        if (is_file($fullpath)) {
             /** @noinspection PhpIncludeInspection */
             require_once $fullpath;
         }
