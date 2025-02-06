756ead2e3660e12683f7bc7cdb7a520b6c1bd9cc
Jan Slabon <jan.slabon@setasign.com>
2025-02-06 10:22:01 +0100

    Update zugferd2p1.php

--- demos/zugferd2p1.php
+++ demos/zugferd2p1.php
@@ -40,7 +40,6 @@ class ZUGFeRD2p1 extends SetaFpdi
             $iccStream
         );
         $outputIntents->addOutputIntent($outputIntent);
-        $document->setPdfVersion('1.7');
     }
 
     public function Output($dest = '', $name = '')
